de9e05f43e4e9417ab5a2f05ac9c52beb1ef0a69
coderxslee <coderxslee@qq.com>
2025-01-30 22:25:56 +0800

    no message

--- book_store/book_store_server/bin/main.dart
+++ book_store/book_store_server/bin/main.dart
@@ -1,11 +1,7 @@
 import 'package:book_store_server/server.dart';
-import 'package:book_store_server/src/generated/protocol.dart';
-import 'package:book_store_shared/book_store_shared.dart';
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
 void main(List<String> args) {
-  // 注册泛型序列化
-  CommonResult.register<SysUser>((json) => SysUser.fromJson(json));
   run(args);
 }
