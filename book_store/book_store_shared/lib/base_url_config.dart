587012817a103cc49dbbece55112a97984192e74
coderxslee <coderxslee@qq.com>
2025-03-02 09:00:03 +0800

    no message

--- book_store/book_store_shared/lib/base_url_config.dart
+++ book_store/book_store_shared/lib/base_url_config.dart
@@ -1,8 +1,8 @@
 
 
-const String BASE_URL = 'http://localhost:8080/';
+// const String BASE_URL = 'http://localhost:8080/';
 
-// const String BASE_URL = 'http://116.205.108.41:8080/';
+const String BASE_URL = 'http://116.205.108.41:8080/';
 
 // 生产环境
 // const String BASE_URL = 'https://www.aiziran.com/';
